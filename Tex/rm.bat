b1242e9c121225ab879570172344442c1f91a117
mishaZ <mz_1350@mail.ru>
2022-06-21 13:43:26 +0300

    Проверка старой прогой теперь проходит

--- Tex/rm.bat
+++ Tex/rm.bat
@@ -1,8 +1,8 @@
-del main.aux
-del main.bbl
-del main.blg
-del main.out
-del main.toc
-del main.log
+del main_super_new.aux
+del main_super_new.bbl
+del main_super_new.blg
+del main_super_new.out
+del main_super_new.toc
+del main_super_new.log
 
-del main.pdf
+del main_super_new.pdf
